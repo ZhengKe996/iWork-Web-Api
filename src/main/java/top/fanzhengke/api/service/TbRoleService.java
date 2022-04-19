48942612f05a83274cc48d804e8cb7491445a8c6
ZhengKe996 <3119531802@qq.com>
2022-04-19 20:34:04 +0800

    feat: 新增XSS过滤

--- src/main/java/top/fanzhengke/api/service/TbRoleService.java
+++ src/main/java/top/fanzhengke/api/service/TbRoleService.java
@@ -1,13 +1,11 @@
 package top.fanzhengke.api.service;
 
-import top.fanzhengke.api.domain.TbRole;
-import com.baomidou.mybatisplus.service.IService;
 
 /**
-* @author coco
-* @description 针对表【tb_role(角色表)】的数据库操作Service
-* @createDate 2022-04-19 19:47:44
-*/
-public interface TbRoleService extends IService<TbRole> {
+ * @author coco
+ * @description 针对表【tb_role(角色表)】的数据库操作Service
+ * @createDate 2022-04-19 19:47:44
+ */
+public interface TbRoleService {
 
 }
--- src/main/java/top/fanzhengke/api/service/TbUserService.java
+++ src/main/java/top/fanzhengke/api/service/TbUserService.java
@@ -1,13 +1,11 @@
 package top.fanzhengke.api.service;
 
-import top.fanzhengke.api.domain.TbUser;
-import com.baomidou.mybatisplus.service.IService;
 
 /**
 * @author coco
 * @description 针对表【tb_user(用户表)】的数据库操作Service
 * @createDate 2022-04-19 19:47:44
 */
-public interface TbUserService extends IService<TbUser> {
+public interface TbUserService {
 
 }
